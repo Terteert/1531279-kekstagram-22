a3b2456205ef467ec40a11c8aae9034efb1eca22
egor66663 <70906247+egor66663@users.noreply.github.com>
2021-02-02 14:56:12 +0300

    исправил
    неуверен правильно ли с throw

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-const randomNumber = function (min, max) {
-  if (min < 0 || max < 0 || min > max) {
-    return 'некорректное значение';
+const getRandomNumber = function (min, max) {
+  if (min >= 0 && max > 0 && min < max) {
+    return Math.floor(Math.random() * (max - min)) + min; // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
   } 
-  return Math.floor(Math.random() * (max - min)) + min; // https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+  throw 'некорректное значение';
 };
     
-randomNumber();
+getRandomNumber();
     
-const commentLength = function (min, max) {
+const acceptCommentLength = function (min, max) {
   if (min > 0 && max <= 140) {
-    return 'true';
+    return true;
   }
-  return 'false';
+  return false;
 };
     
-commentLength();
+acceptCommentLength();
